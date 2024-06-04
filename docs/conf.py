dae1b0e7a8cbdb9529572596291c6843404fb4b4
robin-cls <74916839+robin-cls@users.noreply.github.com>
2024-06-04 09:14:38 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -61,6 +61,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-#html_theme = 'sphinx_rtd_theme'
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+#html_theme = 'alabaster'
 html_static_path = ['_static']
